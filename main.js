acff2fb0e4b1d83837b6c96cee9a5b64e5c18693
Maria <achigarpia@gmail>
2022-05-26 08:58:53 -0300

    desafio5-clase10-hbs

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
-
+const Container = require("./Container");
 //app.use("/static", express.static(`${__dirname}/public`));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const contenedor = new Container();
 const engineFn = engine({
   // registro el engine de hbs
   extname: ".hbs",
@@ -20,29 +21,29 @@ app.engine("hbs", engineFn);
 app.set("views", "./views/hbs");
 app.set("view engine", "hbs");
 
-const productos = [];
+app.get("", (req, res) => {
+  return res.render("layouts/main");
+});
+
+app.get("/listado", async (req, res) => {
+  const productos = await contenedor.getAll();
+  const data = {
+    productos,
+  };
+
+  return res.render("layouts/listado", data);
+});
+
 //con hbs
-app.post("/producto", (req, res) => {
+app.post("/listado", async (req, res) => {
   const producto = {
     nombre: req.body.nombre,
     precio: req.body.precio,
     foto: req.body.foto,
   };
-  productos.push(producto);
+  const id = await contenedor.save(producto);
+  console.log("ID asignado: ", id);
   return res.redirect("/"); //como no va a mostrar nada, lo redirijo al form
-});
-
-app.get("", (req, res) => {
-  return res.render("layouts/main");
-});
-
-app.get("/listado", (req, res) => {
-  const data = {
-    nombre: "escuadra",
-    precio: 1245,
-    foto: "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-512.png",
-  };
-  return res.render("layouts/listado", data);
 });
 
 const PORT = 8080;
