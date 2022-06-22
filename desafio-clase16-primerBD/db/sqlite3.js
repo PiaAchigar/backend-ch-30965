834da7345b4222a33877a0890b99eaacd321d4fa
Maria <achigarpia@gmail>
2022-06-22 16:16:57 -0300

    clear

--- desafio-clase16-primerBD/db/sqlite3.js
+++
@@ -1,7 +0,0 @@
-const options = {
-  client: "sqlite3",
-  connection: { filename: "./mydb.sqlite" }, //no es en formato json, es info binaria no lo puedo abrir con el vsc
-};
-module.exports = {
-  options,
-};
