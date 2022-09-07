1ba1d9393672bd16a77cfe5cbc5f91e69639a87f
Maria <achigarpia@gmail>
2022-09-07 11:25:55 -0300

    tercer-preentrega

--- utils/sendWhatsapp.js
+++ utils/sendWhatsapp.js
@@ -1,8 +1,11 @@
 const twilio = require("twilio");
 
-const ACCOUNT_SID = "ACe87fed0ac4660dd435107afba7ea4ee6";
-const AUTH_TOKEN = "c2c7e6d6265de9cd96730445c5ec446b";
-const PHONE_NUMBER = "whatsapp:+14155238886";
+// const ACCOUNT_SID = "ACe87fed0ac4660dd435107afba7ea4ee6";
+// const AUTH_TOKEN = "c2c7e6d6265de9cd96730445c5ec446b";
+// const PHONE_NUMBER = "whatsapp:+14155238886";
+const ACCOUNT_SID = "ACb81319c28fcecad693735793d292c679";
+const AUTH_TOKEN = "b52b615a70c8af7c4966beb4332c3720";
+const PHONE_NUMBER = "whatsapp:+12184132907";
 
 const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 
