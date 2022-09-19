edb43ed241a5e061649bebeed9b36a25477067f1
Maria <achigarpia@gmail>
2022-09-18 22:10:29 -0300

    mongoAtlas

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const userRouter = require("./Routers/userRouter");
 
 //como me conecto a la bd de mongoAtlas??
 //mongoose.connect("mongodb://localhost:27017/30965-clase38");
-mongoose.connect("mongodb+srv://cluster0.5z86n.mongodb.net/myFirstDatabase");
+mongoose.connect(
+  "mongodb+srv://pia:EyBuOppa7XvDu65z@cluster0.5z86n.mongodb.net/?retryWrites=true&w=majority"
+);
 
 const app = express();
 
